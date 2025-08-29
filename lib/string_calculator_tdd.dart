0f5b62e58e5c11f7a30232c44bcb1ab514c9deba
premsingh01 <premsingh7107@gmail.com>
2025-08-30 01:17:06 +0530

    green: handling 1\n2,3 return 6

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -2,7 +2,8 @@ int add(String numbers) {
   if(numbers.isEmpty) {
     return 0;
   } else {
-    final numberList = numbers.split(',').map((e) => int.tryParse(e) ?? 0).toList();
+    final String numbersWithComma = numbers.replaceAll("\n", ",");
+    final numberList = numbersWithComma.split(',').map((e) => int.tryParse(e) ?? 0).toList();
     return numberList.reduce((a, b) => a + b);
   } 
 }
