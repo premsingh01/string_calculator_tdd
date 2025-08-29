9dab35d6ac8da828df5603be67123121ad91c977
premsingh01 <premsingh7107@gmail.com>
2025-08-29 23:56:29 +0530

    red: failing test for 1

--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -5,4 +5,7 @@ void main() {
   test('Should get 0 in case of empty string', () {
     expect(add(''), 0);
   });
+  test('Should get 1 in case of string is 1', () {
+    expect(add('1'), 1);
+  });
 }
