ee670f0faaa765e35f84e507156cc184ef260d3a
premsingh01 <premsingh7107@gmail.com>
2025-08-30 02:34:35 +0530

    green: throw exception in case string contains negative numbers

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -2,13 +2,17 @@ int add(String numbers) {
   if(numbers.isEmpty) {
     return 0;
   } else {
-    String delimeter = '';
+    String delimeter = ',|\n';
     if(numbers.startsWith("//")) {
       final part = numbers.split("\n");
       delimeter = part.first.substring(2);
       numbers = part.last;
-    }    
-    final numberList = numbers.split(RegExp(',|\n|$delimeter')).map((e) => int.tryParse(e) ?? 0).toList();
+    }
+    final numberList = numbers.split(RegExp(delimeter)).map((e) => int.tryParse(e) ?? 0).toList();
+    final List negativeNumberList = numberList.where((element) => element<0).toList();
+    if(negativeNumberList.isNotEmpty) {
+      throw Exception("Negative Number not allowed: ${negativeNumberList.join(", ")}");
+    }
     return numberList.reduce((a, b) => a + b);
   } 
 }
