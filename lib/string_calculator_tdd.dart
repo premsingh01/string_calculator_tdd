62d1722c38cc3457c427cba1a53894171d9ea5f8
premsingh01 <premsingh7107@gmail.com>
2025-08-30 01:27:28 +0530

    refactor: using RegExp for string separation

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -2,8 +2,7 @@ int add(String numbers) {
   if(numbers.isEmpty) {
     return 0;
   } else {
-    final String numbersWithComma = numbers.replaceAll("\n", ",");
-    final numberList = numbersWithComma.split(',').map((e) => int.tryParse(e) ?? 0).toList();
+    final numberList = numbers.split(RegExp(',|\n')).map((e) => int.tryParse(e) ?? 0).toList();
     return numberList.reduce((a, b) => a + b);
   } 
 }
